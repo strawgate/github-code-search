193bf828eac042e08655e95afdae5aba54a32101
William Easton <bill.easton@elastic.co>
2025-09-30 16:52:50 -0500

    add clone dir var

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from logging import Logger
 
 
+import os
 from pathlib import Path
+from tempfile import mkdtemp
 from fastmcp.server.server import FastMCP
 from git import Repo
 
 
-from fastmcp import FastMCP
 from fastmcp.utilities.logging import get_logger
 
+clone_dir = Path(os.environ.get("CLONE_DIR", Path.cwd() / "temp"))
+
+clone_dir.mkdir(parents=True, exist_ok=True)
 
 mcp: FastMCP[None] = FastMCP[None](name="github-code-search")
 
@@ -16,14 +20,20 @@ cloned_repositories: dict[str, Path] = {}
 
 logger: Logger = get_logger(name=__name__)
 
+
 def clone_github_repository(owner: str, repo: str, directory: str) -> Path:
     """Clone a git repository to a temporary directory."""
+    directory = mkdtemp(prefix=f"{owner}_{repo}", dir=clone_dir)
+
     if f"{owner}/{repo}" in cloned_repositories:
         return cloned_repositories[f"{owner}/{repo}"]
+
     logger.info(f"Cloning repository {owner}/{repo} to {directory}")
+
     _ = Repo.clone_from(
         f"https://github.com/{owner}/{repo}.git", directory, depth=1, single_branch=True
     )
+
     cloned_repositories[f"{owner}/{repo}"] = Path(directory)
     logger.info(f"Cloned repository {owner}/{repo} to {directory}")
     return Path(directory)
@@ -36,6 +46,7 @@ def search_code(owner: str, repo: str, path: str) -> str:
     if not file.exists():
         raise ValueError(f"File {path} not found in repository {repo}")
     return file.read_text()
+
 
 if __name__ == "__main__":
     mcp.run(transport="sse")
